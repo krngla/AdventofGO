f767d5ef54ea828dfcd1277a94310df0dbe48387
kringla <krngle@gmail.com>
2023-08-10 16:37:04 +0200

    Added flag parameters to test

--- test/go.mod
+++ test/go.mod
@@ -1,3 +1,5 @@
 module github.com/krngla/AdventofGO/test
 
 go 1.20
+
+require github.com/krngla/AdventofGO/meta v0.0.1
--- test/main.go
+++ test/main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"github.com/krngla/AdventofGO/meta"
 	"fmt"
+	"flag"
 )
 
 
 func main () {
-	url := "https://adventofcode.com/2022/day/4/input"
-	session := ""
+	//dayPtr := flag.Int("d", 0, "select day")
+	sessPtr := flag.String("s", "", "Session id")
 
-	b, err := meta.HTTPwithCookies(url, session)
-	if err != nil {
-		fmt.Println("FAILED")
+	flag.Parse()
+
+	url := "https://adventofcode.com/2022/day/4/input"
+	session := *sessPtr
+	if (*sessPtr != "") {
+
+		b, err := meta.HTTPwithCookies(url, session)
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		fmt.Println(string(b))
+	} else {
+		fmt.Println("No session ID provided")
 	}
-	fmt.Println(string(b))
 }
