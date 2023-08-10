9dbef1a22c173f7c4973f5c69d8c1ad631222dbf
kringla <krngle@gmail.com>
2023-08-10 16:54:29 +0200

    added cli to day1

--- day1/go.mod
+++ day1/go.mod
@@ -1,3 +1,5 @@
 module projects/adventofcode/go/day1
 
 go 1.20
+
+require github.com/krngla/AdventofGO/meta v0.0.1
--- day1/main.go
+++ day1/main.go
@@ -5,22 +5,18 @@ import (
 	"fmt"
 	"strconv"
 	"os"
+	"strings"
+	"github.com/krngla/AdventofGO/meta"
+	"flag"
 )
 
 
 
-func main() {
-	f, err := os.Open("calories");
-	if(err != nil) {
-		os.Exit(-1);
-	}
-
-
-	scanner := bufio.NewScanner(f);
+func doTask(i *bufio.Scanner) {
 	elfn := 0
 	elfc := []int64{0}
-	for scanner.Scan() {
-		line := scanner.Text()
+	for i.Scan() {
+		line := i.Text()
 		if(line == "") {
 			elfn++;
 			elfc = append(elfc, 0)
@@ -42,4 +38,38 @@ func main() {
 	}
 
 	fmt.Printf("Elf with most calories is elf %d, which carries %d calories\n", elfi, elfm);
+}
+
+func main() {
+	sessPtr := flag.String("s", "", "Session id")
+
+	flag.Parse()
+
+	url := "https://adventofcode.com/2022/day/1/input"
+	session := *sessPtr
+	inputs := ""
+	var input *bufio.Scanner
+	if (*sessPtr != "") {
+
+		b, err := meta.HTTPwithCookies(url, session)
+		if err != nil {
+			fmt.Println(err)
+		}
+			
+		inputs = string(b)
+		input = bufio.NewScanner(strings.NewReader(inputs))
+
+	}
+
+	f, err := os.Open("calories");
+	if(err != nil) {
+		os.Exit(-1);
+	}
+
+
+	scanner := bufio.NewScanner(f);
+	doTask(scanner)
+	if (inputs != "") {
+		doTask(input)
+	}
 }
