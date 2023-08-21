8db0abea4e9f16503eab9038851c419a1373e90f
kringla <krngle@gmail.com>
2023-08-21 13:20:47 +0200

    Added stdin support for day 3 and embedded testcase

--- day3/main.go
+++ day3/main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	//	"string"
 	"bufio"
 	"fmt"
-	"k8s.io/klog"
 	"os"
+	"strings"
 )
 
+var testans int = 157
+var teststr string = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+`
 
-func main() {
-	klog.InitFlags(nil)
-	f, err := os.Open("racksacks")
-	if err != nil {
-		os.Exit(-1)
-	}
-
-	scanner := bufio.NewScanner(f)
+func doTask(i *bufio.Scanner) int {
 	totprio := 0
-	for scanner.Scan() {
-		line := scanner.Text()
+	for i.Scan() {
+		line := i.Text()
 
 		linelen := len(line)
 		linehalf := int(linelen / 2)
 		compA := line[0:linehalf]
-		compB := line[linehalf : linelen]
-	//	klog.Infof("Line: %s, Linelen: %d, linehalf: %d\n\tcompA: %s, len: %d\n\tcompB: %s, len: %d\n", line, linelen, linehalf, compA, len(compA), compB, len(compB))
+		compB := line[linehalf:linelen]
+		//	klog.Infof("Line: %s, Linelen: %d, linehalf: %d\n\tcompA: %s, len: %d\n\tcompB: %s, len: %d\n", line, linelen, linehalf, compA, len(compA), compB, len(compB))
 		m := make(map[rune]int)
 
 		offset := 'A' - 1
@@ -38,7 +38,7 @@ func main() {
 			if collision != 0 {
 				m[c] = 0
 				cprio := (c - offset)
-				if cprio > ('a' - offset) { 
+				if cprio > ('a' - offset) {
 					cprio -= (loffset - offset)
 				} else {
 					cprio += ('Z' - offset)
@@ -46,8 +46,31 @@ func main() {
 				fmt.Println(cprio, string(c))
 				totprio += int(cprio)
 			}
-		}	
+		}
 
 	}
 	fmt.Println(totprio)
+	return totprio
+}
+
+func main() {
+
+	scanner := bufio.NewScanner(strings.NewReader(teststr))
+	result := doTask(scanner)
+	if result != testans {
+		fmt.Printf("Failed test, expected %d, got %d\n", testans, result)
+		os.Exit(-1)
+	}
+
+	instream := os.Stdin
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Println("instream.Stat()", err)
+	}
+	if fi.Mode()&os.ModeNamedPipe == 0 {
+		fmt.Println("NoInput")
+	} else {
+		ioScan := bufio.NewScanner(instream)
+		doTask(ioScan)
+	}
 }
